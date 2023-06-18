f7a49258fe1f3ea37efbd1081b9bf9d9f470fe87
Sonia Rodrigues <soniaalexandraa94@gmail.com>
2023-06-18 18:14:11 +0100

    Created the library shared with the cryptography provider and the model result

---
+++ SharePix/SharePix.Shared/Models/Result.cs
@@ -0,0 +1,9 @@
+﻿namespace SharePix.Shared.Models
+{
+    public class Result<T>
+    {
+        public string SuccessMessage { get; set; }
+        public string ErrorMessage { get; set; }
+        public T Object { get; set; }
+    }
+}
