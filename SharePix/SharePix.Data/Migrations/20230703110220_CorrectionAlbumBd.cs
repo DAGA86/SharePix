66b91692c20ecdb102c69483bd3a471cd7160b65
Sonia Rodrigues <100570965+cosmicsanr@users.noreply.github.com>
2023-07-04 08:43:31 +0100

    Correction the model Album field and update database

---
+++ SharePix/SharePix.Data/Migrations/20230703110220_CorrectionAlbumBd.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SharePix.Data.Migrations
+{
+    public partial class CorrectionAlbumBd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Photo_Album_AlbumId",
+                table: "Photo");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AlbumId",
+                table: "Photo",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Photo_Album_AlbumId",
+                table: "Photo",
+                column: "AlbumId",
+                principalTable: "Album",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Photo_Album_AlbumId",
+                table: "Photo");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AlbumId",
+                table: "Photo",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Photo_Album_AlbumId",
+                table: "Photo",
+                column: "AlbumId",
+                principalTable: "Album",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
