acf2de82a7ab66875b8a900d7d747fb3dc303896
Sonia Rodrigues <100570965+cosmicsanr@users.noreply.github.com>
2023-07-07 10:33:33 +0100

    Changed property Description to nullable on model Album

--- SharePix/SharePix.Data/Models/Album.cs
+++ SharePix/SharePix.Data/Models/Album.cs
@@ -14,7 +14,7 @@ namespace SharePix.Data.Models
         [StringLength(32)]
         public string Name { get; set; }
         [StringLength(256)]
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public DateTime CreateDate { get; set; }
         public int OwnerId { get; set; }
 
