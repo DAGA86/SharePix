6d7d5adf8c178e3a7f211c48414da25530ec3442
Sonia Rodrigues <100570965+cosmicsanr@users.noreply.github.com>
2023-07-07 16:28:50 +0100

    Created view models albums and photos homepage

---
+++ SharePix/SharePix.WebApp/Models/Albums/AlbumViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SharePix.WebApp.Models.Albums
+{
+    public class AlbumViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int OwnerId { get; set; }
+    }
+}
---
+++ SharePix/SharePix.WebApp/Models/HomePage/PhotosViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SharePix.WebApp.Models.HomePage
+{
+    public class PhotosViewModel
+    {
+        public int Id { get; set; }
+        public int OwnerId { get; set; }
+        public DateTime? Date { get; set; }
+        public string? Location { get; set; }
+        public string? Description { get; set; }
+        public int? AlbumId { get; set; }
+
+		public Photos.UploadPhotoViewModel[] UploadPhoto { get; set; } = new Photos.UploadPhotoViewModel[0];
+	}
+}
