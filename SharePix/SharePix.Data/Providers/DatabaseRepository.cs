8e19696c05d058900ed4d53c3f3be1b659f24e28
Sonia Rodrigues <100570965+cosmicsanr@users.noreply.github.com>
2023-07-11 08:10:14 +0100

    Update provider Database with a new filter expression

--- SharePix/SharePix.Data/Providers/DatabaseRepository.cs
+++ SharePix/SharePix.Data/Providers/DatabaseRepository.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Options;
 using Org.BouncyCastle.Crypto;
 using SharePix.Data.Contexts;
 using SharePix.Data.Models;
+using SharePix.Shared.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,12 +15,41 @@ namespace SharePix.Data.Providers
 {
     public class DatabaseRepository
     {
-            private readonly DatabaseContext _context;
+        private readonly DatabaseContext _context;
 
-            public DatabaseRepository(DatabaseContext context)
+        public DatabaseRepository(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public Result<List<TViewModel>> Get<TEntity, TViewModel>(
+            Expression<Func<TEntity, bool>>? filterExpression,
+            Expression<Func<TEntity, TViewModel>>? projection)
+            where TEntity : class
+        {
+            Result<List<TViewModel>> result = new Result<List<TViewModel>>();
+            try
             {
-                _context = context;
+                IQueryable<TEntity> query = _context.Set<TEntity>();
+                if (filterExpression != null) 
+                {
+                    query = query.Where(filterExpression);
+                }
+                if (projection != null)
+                {
+                    result.Object = query.Select(projection).ToList();
+                }
+                else
+                {
+                    result.Object = query.Cast<TViewModel>().ToList();
+                }
             }
+            catch (Exception exception)
+            {
+                result.ErrorMessage = $"{nameof(Get)} - {exception.Message}";
+            }
+            return result;
+        }
 
 
         public List<T> GetAllFiltered<T>(Expression<Func<T, bool>> filterExpression) where T : class
@@ -29,9 +59,9 @@ namespace SharePix.Data.Providers
 
         //change bool to int
         public T GetFirstFiltered<T>(Expression<Func<T, bool>> filterExpression) where T : class
-            {
-                return _context.Set<T>().FirstOrDefault();
-            }
+        {
+            return _context.Set<T>().FirstOrDefault();
+        }
 
         public void DeleteFiltered<T>(Expression<Func<T, bool>> filterExpression) where T : class
         {
