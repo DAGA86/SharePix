c9e8f8867f7763f1238332133839138e9585dcd1
Sonia Rodrigues <100570965+cosmicsanr@users.noreply.github.com>
2023-07-12 11:59:18 +0100

    Created the methods, controllers and views to create albuns
    Added lightbox on images;

--- SharePix/SharePix.WebApp/Controllers/AlbumsController.cs
+++ SharePix/SharePix.WebApp/Controllers/AlbumsController.cs
@@ -1,25 +1,56 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using SharePix.Data.Contexts;
+using SharePix.Data.Models;
 using SharePix.Data.Providers;
+using SharePix.Shared.Models;
 using SharePix.WebApp.Models.Albums;
+using SharePix.WebApp.Models.HomePage;
 using System.Security.Claims;
 
 namespace SharePix.WebApp.Controllers
 {
+    [Authorize]
     public class AlbumsController : BaseController
     {
         private AlbumProvider _albumProvider;
         private readonly IWebHostEnvironment _env;
         private PhotoProvider _photoProvider;
+        private DatabaseRepository _databaseRepository;
 
 
         public AlbumsController(DatabaseContext context, IWebHostEnvironment env, LanguageProvider languageProvider, LocalizationProvider localizationProvider) : base(languageProvider, localizationProvider)
         {
             _albumProvider = new AlbumProvider(context);
             _photoProvider = new PhotoProvider(context);
+            _databaseRepository = new DatabaseRepository(context);
             _env = env;
 
         }
+
+        public IActionResult Index(int id)
+        {
+            int userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            Result<List<AlbumPhotoViewModel>> resultPhotos = _databaseRepository.Get<Photo, AlbumPhotoViewModel>(
+                x => x.AlbumId == id && x.OwnerId == userId,
+                x => new AlbumPhotoViewModel { Date = x.Date, Description = x.Description, Id = x.Id, Location = x.Location, UploadDate = x.UploadDate}
+                );
+
+            Result<List<AlbumViewModel>> resultAlbum = _databaseRepository.Get<Album, AlbumViewModel>(
+                x => x.Id == id && x.OwnerId == userId,
+                x => new AlbumViewModel { Id = x.Id, Name = x.Name, Description = x.Description, CreateDate = x.CreateDate}
+                );
+
+            if (resultAlbum.Object != null && resultAlbum.Object.Any())
+            {                
+                AlbumViewModel viewModel = resultAlbum.Object.First();
+                viewModel.Photos = resultPhotos.Object;
+                return View(viewModel);
+            }
+
+            return RedirectToAction(nameof(Index), "Home");
+        }
+
 
         public IActionResult Create()
         {
@@ -81,7 +112,7 @@ namespace SharePix.WebApp.Controllers
                                     OwnerId = userId,
                                     Description = model.Description,
                                     AlbumId = album.Id,
-                                    
+
                                 };
                                 var newFileName = $"{_photoProvider.Create(photo).Id}.jpg";
 
@@ -105,6 +136,6 @@ namespace SharePix.WebApp.Controllers
 
         }
 
-        }
     }
+}
 
---
+++ SharePix/SharePix.WebApp/Models/Albums/AlbumViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SharePix.WebApp.Models.Albums
+{
+    public class AlbumViewModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "field.required")]
+        [MaxLength(32, ErrorMessage = "album.name.maxLength")]
+        [MinLength(1, ErrorMessage = "album.name.minLength")]
+        public string Name { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public string? Description { get; set; }
+        public List<AlbumPhotoViewModel> Photos { get; set; }
+    }   
+
+    public class AlbumPhotoViewModel
+    {
+        public int Id { get; set; }
+        public DateTime? Date { get; set; }
+        public DateTime? UploadDate { get; set; }
+        public string? Location { get; set; }
+        public string? Description { get; set; }
+    }
+}
