2b8d69fa3066260b637bf468e787988ae6862634
Sonia Rodrigues <100570965+cosmicsanr@users.noreply.github.com>
2023-07-26 14:51:41 +0100

    Fix filter photo and album

--- SharePix/SharePix.WebApp/wwwroot/js/homepage.js
+++ SharePix/SharePix.WebApp/wwwroot/js/homepage.js
@@ -1,4 +1,21 @@
 ﻿$(document).ready(function () {
+    $('#photos, #albums').click(function () {
+        let toShow = [];
+        if ($('#photos').is(':checked')) {
+            toShow.push(".photo");
+        }
+        if ($('#albums').is(':checked')) {
+            toShow.push(".album");
+        }
+        if (toShow.length > 0)
+            $('.portfolio_isotope_container').isotope({ filter: toShow.join() });
+        else
+            $('.portfolio_isotope_container').isotope({ filter: '.none' });
+    });
+});
+
+
+$(document).ready(function () {
     setTimeout(function () {
         $("#successAlert").alert("close");
     }, 6000);
